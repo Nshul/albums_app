989aac12963110dfd124c19bc310c39c5d801a92
Anshul Mittal <anshulmittal0409@gmail.com>
2017-12-05 22:33:57 +0530

    Worked till before http req

---
+++ src/components/AlbumList.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class AlbumList extends Component {
+    render() {
+        return (
+            <Text>Dummy</Text>
+        );
+    }
+};
---
+++ src/components/header.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <View style={styles.viewbox}>
+            <Text style={styles.textStyles} >{ this.props.name }</Text>
+            </View>
+        );
+    }
+} 
+
+const styles = StyleSheet.create({
+    textStyles: {
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
+    viewbox: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 60,
+        // paddingTop: 15,
+        // paddingBottom: 15,
+        backgroundColor: '#F8F8F8',
+        // Shadow Works for ios only
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 20 },
+        shadowOpacity: 0.2,
+        // Android 5.0+ support elevation
+        elevation: 9,
+    }
+});
+
