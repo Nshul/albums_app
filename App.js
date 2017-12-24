d42595185bf1334e66c08c9a93f72c9564d1a4dc
Anshul Mittal <anshulmittal0409@gmail.com>
2017-12-24 12:51:20 +0530

    completed basic albums app

--- App.js
+++ App.js
@@ -5,21 +5,15 @@
  */
 
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  Image
-} from 'react-native';
+import { View } from 'react-native';
 import Header from './src/components/header';
 import AlbumList from './src/components/AlbumList';
 
 export default class App extends Component {
   render() {
     return (
-        <View> 
-          <Header name="Albums!!" />
+        <View style={{ flex: 1 }} > 
+          <Header name={'Albums'} />
           <AlbumList />
         </View>
     );
--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+    const { title, artist, thumbnail_image, image, url } = album;
     return (
         <Card>
             <CardSection>
-                <View></View>
-                <View style={styles.headerContentStyle}>
-                    <Text>{props.album.title}</Text>
-                    <Text>{props.album.artist}</Text>
+                <View style={styles.thumbnailContainerStyle}>
+                    <Image style={styles.thumbnailStyle} source={{ uri: thumbnail_image }} />
                 </View>
+                <View style={styles.headerContentStyle}>
+                    <Text style={styles.headerTextStyle} >{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>
+
+            <CardSection>
+                <Image style={styles.imageStyle} source={{ uri: image }} />
+            </CardSection>
+
+            <CardSection>
+                <Button onPress={() => Linking.openURL(url)} >
+                    Buy Now
+                </Button>
             </CardSection>
         </Card>
     );
@@ -21,6 +35,24 @@ const styles = StyleSheet.create({
     headerContentStyle: {
         flexDirection: 'column',
         justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+        fontSize: 18
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
     }
 });
 
