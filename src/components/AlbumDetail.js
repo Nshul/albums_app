0fdd6437d3b8e06b29a9aaedf53a84dcb28a33d5
Anshul Mittal <anshulmittal0409@gmail.com>
2017-12-24 10:52:09 +0530

    till 43 number vid

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
 
 const AlbumDetail = (props) => {
     return (
         <Card>
-            <Text>{props.album.title}</Text>
+            <CardSection>
+                <View></View>
+                <View style={styles.headerContentStyle}>
+                    <Text>{props.album.title}</Text>
+                    <Text>{props.album.artist}</Text>
+                </View>
+            </CardSection>
         </Card>
     );
 };
+
+const styles = StyleSheet.create({
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    }
+});
 
 export default AlbumDetail;
